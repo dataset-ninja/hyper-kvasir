8a54b7c8a4073bd5f87214dde43abc812362497e
Ars Vladimirov <grokhael@gmail.com>
2023-07-21 16:14:09 +0600

    upd custom data

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,9 +1,7 @@
-**HyperKvasir: The Largest Gastrointestinal Dataset** is a dataset for instance segmentation, semantic segmentation, and object detection tasks. It is used in the medical industry.
+**HyperKvasir: The Largest Gastrointestinal Dataset** is a dataset for instance segmentation, semantic segmentation, and object detection tasks. It is used in the medical industry. 
 
-The dataset consists of 1000 images with 2133 labeled objects belonging to 2 different classes including *polyp* and *polyp_bbox*.
+The dataset consists of 1000 images with 2135 labeled objects belonging to 2 different classes including *polyp* and *polyp_bbox*.
 
-Images in the HyperKvasir dataset have pixel-level instance segmentation and bounding box annotations. Due to the nature of the instance segmentation task, it can be automatically transformed into a semantic segmentation task (only one mask for every class). All images are labeled (i.e. with annotations). There is 1 split in the dataset: *ds0* (1000 images). The dataset was released in 2020 by the [NO-SW-AU joint research group](https://www.nature.com/articles/s41597-020-00622-y#author-information).
+Images in the HyperKvasir dataset have pixel-level instance segmentation and bounding box annotations. Due to the nature of the instance segmentation task, it can be automatically transformed into a semantic segmentation task (only one mask for every class). All images are labeled (i.e. with annotations). There are no pre-defined <i>train/val/test</i> splits in the dataset. The dataset was released in 2020 by the [NO-SW-AU joint research group](https://www.nature.com/articles/s41597-020-00622-y#author-information).
 
-Here is the visualized example grid with annotations:
-
-<img src="https://github.com/dataset-ninja/hyper-kvasir/raw/main/visualizations/horizontal_grid.png">
+<img src="https://github.com/dataset-ninja/hyper-kvasir/raw/main/visualizations/poster.png">
--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,14 @@
 from typing import Dict, List, Optional, Union
 
-from dataset_tools.templates import AnnotationType, CVTask, Industry, License
+from dataset_tools.templates import (
+    AnnotationType,
+    Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
+)
 
 ##################################
 # * Before uploading to instance #
@@ -12,7 +20,9 @@ PROJECT_NAME_FULL: str = "HyperKvasir: The Largest Gastrointestinal Dataset"
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.CC_BY_4_0()
-INDUSTRIES: List[Industry] = [Industry.Medical()]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Medical()]
+CATEGORY: Category = Category.Medical()
+
 CV_TASKS: List[CVTask] = [
     AnnotationType.InstanceSegmentation(),
     CVTask.SemanticSegmentation(),
@@ -23,7 +33,9 @@ ANNOTATION_TYPES: List[AnnotationType] = [
     AnnotationType.ObjectDetection(),
 ]
 
-RELEASE_YEAR: int = 2020
+RELEASE_DATE: Optional[str] = "2020-08-28"  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = None
 HOMEPAGE_URL: str = "https://datasets.simula.no/hyper-kvasir/"
 # e.g. "https://some.com/dataset/homepage"
 
@@ -46,10 +58,33 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 PAPER: Optional[str] = "https://www.nature.com/articles/s41597-020-00622-y"
 CITATION_URL: Optional[str] = "https://datasets.simula.no/hyper-kvasir/"
+AUTHORS: Optional[List[str]] = [
+    "Hanna Borgli",
+    "Vajira Thambawita",
+    "Pia H. Smedsrud",
+    "Steven Hicks",
+    "Debesh Jha",
+    "Sigrun L. Eskeland",
+    "Kristin Ranheim Randel",
+    "Konstantin Pogorelov",
+    "Mathias Lux",
+    "Duc Tien Dang Nguyen",
+    "Dag Johansen",
+    "Carsten Griwodz",
+    "Håkon K. Stensland",
+    "Enrique Garcia-Ceja",
+    "Peter T. Schmidt",
+    "Hugo L. Hammer",
+    "Michael A. Riegler",
+    "Pål Halvorsen",
+    "Thomas de Lange",
+]
+
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "NO-SW-AU joint research group"
 ORGANIZATION_URL: Optional[
     Union[str, List[str]]
 ] = "https://www.nature.com/articles/s41597-020-00622-y#author-information"
+SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
 TAGS: List[str] = None
 
 ##################################
@@ -64,10 +99,15 @@ def check_names():
 
 
 def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
-        "industries": INDUSTRIES,
+        "applications": APPLICATIONS,
+        "category": CATEGORY,
         "cv_tasks": CV_TASKS,
         "annotation_types": ANNOTATION_TYPES,
         "release_year": RELEASE_YEAR,
@@ -79,13 +119,16 @@ def get_settings():
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
+    settings["release_date"] = RELEASE_DATE
     settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
 
     return settings
