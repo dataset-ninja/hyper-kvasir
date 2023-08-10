eeed8f976d2366b3d3382df849cd7fb6b7cf8b9b
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-10 19:44:30 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "HyperKvasir Segmentation"
 PROJECT_NAME_FULL: str = "HyperKvasir: The Largest Gastrointestinal Dataset (Segmentation Part)"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -105,6 +106,7 @@ def get_settings():
 
     settings = {
         "project_name": PROJECT_NAME,
+        "hide_dataset": HIDE_DATASET,
         "license": LICENSE,
         "applications": APPLICATIONS,
         "category": CATEGORY,
