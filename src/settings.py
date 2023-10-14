e9f857cff292e458034da7a20ebb8962b5711cd9
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-10-14 22:40:02 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -20,7 +20,7 @@ HIDE_DATASET = False  # set False when 100% sure about repo quality
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = License.CC_BY_4_0()
+LICENSE: License = License.CC_BY_4_0(source_url="https://osf.io/dqam3")
 APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Medical()]
 CATEGORY: Category = Category.Medical(sensitive_content=True)
 
